78689e4520138411640a1a3049a4a71bd8b9d7cb
TrEtLiz <ihellsing57@gmail.com>
2024-11-18 20:44:54 +0200

    add distance.go

---
+++ distance.go
@@ -0,0 +1,8 @@
+package main
+
+// distance function to calculate the distance between two points
+func distance(x1, y1, x2, y2 float64) float64 {
+	a := x2 - x1
+	b := y2 - y1
+	return math.Sqrt(a*a + b*b)
+}
--- main.go
+++ main.go
@@ -5,14 +5,6 @@ import (
 	"math"
 )
 
-// distance function to calculate the distance between two points
-func distance(x1, y1, x2, y2 float64) float64 {
-	a := x2 - x1
-	b := y2 - y1
-	return math.Sqrt(a*a + b*b)
-}
-
-
 func main() {
 	// calling the distance function
 	fmt.Println(distance(0, 0, 3, 4))
